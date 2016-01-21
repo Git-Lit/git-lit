86707bbbeaaf953b290b9217fa40d51b14d0527f
Tom Morris <tfmorris@gmail.com>
2016-01-21 01:16:22 -0500

    Add text file to repo. Skip XML files. Clean up.

--- local.py
+++ local.py
@@ -28,19 +28,25 @@ class CdContext():
     def __exit__(self, exception_type, exception_value, traceback):
         sh.cd(self._og_directory)
 
-IGNORE_FILES = ""
 
 class LocalRepo():
 
     def __init__(self, book):
         self.book = book
         logging.info("Now attempting to initialize a local git repository for text: " 
-                      + self.book.ID + " a.k.a. " + self.book.title )
+                      + self.book.book_id + " a.k.a. " + self.book.title )
 
     def add_file(self, filename):
         sh.git('add', filename)
 
     def add_all_files(self):
+        SUFFIXES = {'_dat.zip',
+                    '_metadata.xml',
+                    'README.md',
+                    'CONTRIBUTING.md',
+                    'LICENSE.md',
+                    '_plain.txt'
+                    }
         with CdContext(self.book.textdir):
             sh.git.init('.')
 
@@ -48,11 +54,16 @@ class LocalRepo():
 
             # NOTE: repo.untracked_files is unreliable with CdContext
             # using sh.ls() instead, this doesn't recognize .gitignore
-            for _file in sh.ls():
-                for _subpath in _file.split():
-                    logging.info("Adding file: " + str(_file))
-
-                    self.add_file(_subpath)
+            for _file in sh.ls('-1'):
+                # TODO: This attempts to add existing files a second time
+                _file = _file.rstrip('\n')
+                for suffix in SUFFIXES:
+                    if _file.endswith(suffix):
+                        logging.info("Adding file: " + _file)
+                        self.add_file(_file)
+                        break
+                else:
+                    logging.debug('Skipping ' + _file)
 
     def commit(self, message):
         with CdContext(self.book.textdir):
@@ -65,7 +76,7 @@ class LocalRepo():
                 )
             except sh.ErrorReturnCode_1 as e: 
                 print("Commit aborted for {0} with msg {1}".format(self.book.book_id, message))
-                print("Error: " + e.value)
+                print("Error: " + e.message)
 
 
 class NewFilesHandler():
@@ -79,14 +90,20 @@ class NewFilesHandler():
     def add_new_files(self):
         self.template_readme()
         self.copy_files()
+        self.write_text()
 
+    def write_text(self):
+        f = codecs.open(self.book.textdir+'/'+self.book.book_id + '_plain.txt','w','utf-8')
+        f.write(self.book.text + '\n')
+        f.close()
+        
     def template_readme(self):
-        templateFile = open('templates/README.md.j2').read()
+        templateFile = codecs.open('templates/README.md.j2','r','utf-8').read()
         template = jinja2.Template(templateFile)
         readme_text = template.render(
             title=self.book.title,
             author=self.book.author,
-            book_id=self.book.ID
+            book_id=self.book.book_id
         )
 
         readme_path = "{0}/{1}".format(
@@ -98,25 +115,31 @@ class NewFilesHandler():
 
     def copy_files(self):
         """ Copy the LICENSE and CONTRIBUTING files to each folder repo """
-        files = ['LICENSE.md', 'CONTRIBUTING.md']
+        # TODO: Add .gitattributes for line endings (and .gitignore?)
+        FILES = ['LICENSE.md', 'CONTRIBUTING.md']
         this_dir = sh.pwd().strip()
-        for _file in files:
+        for _file in FILES:
             sh.cp(
                 '{0}/templates/{1}'.format(this_dir, _file),
                 '{0}/'.format(self.book.textdir)
             )
 
+
 def make(book):
     # Initial commit of book files
     local_repo = LocalRepo(book)
     local_repo.add_all_files()
-    local_repo.commit("initial import from British Library originals.")
+    local_repo.commit("Initial import from British Library originals.")
 
     # New files commit
     NewFilesHandler(book)
 
     local_repo.add_all_files()
-    local_repo.commit("add readme, contributing and license files to book repo")
-    
-#for text in c.texts: 
-#    make(text)
+    local_repo.commit("Add readme, contributing and license files")
+
+
+def test():
+    pass
+
+if __name__ == '__main__':
+    test()
