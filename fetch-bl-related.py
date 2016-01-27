686df080e4c616f147587bf8b420e44baa94d88d
Tom Morris <tfmorris@gmail.com>
2016-01-27 17:32:24 -0500

    Utility to create paper<->digital crosswalk

---
+++ fetch-bl-related.py
@@ -0,0 +1,59 @@
+'''
+Created on Jan 27, 2016
+
+@author: tfmorris
+'''
+
+from lxml import etree as ET
+from lxml.cssselect import CSSSelector
+import requests
+import requests_cache
+import time
+
+URL_TEMPLATE = 'http://primocat.bl.uk/F/?func=direct&local_base=PRIMO&doc_number=%s'
+
+
+def make_throttle_hook(timeout=1.0):
+    """
+    Returns a response hook function which sleeps for `timeout` seconds if
+    response is not cached
+    """
+    def hook(response, *args, **kw_args):
+        if not getattr(response, 'from_cache', False):
+            #print 'sleeping'
+            time.sleep(timeout)
+        return response
+    return hook
+
+def main():
+    requests_cache.install_cache("british-library-catalog")
+    session = requests_cache.CachedSession()
+    session.hooks = {'response': make_throttle_hook(0.5)}
+
+    print('\t'.join(['Print Id', 'Scan Id']))
+
+    with open('metadata/booklist.tsv') as input:
+        for line in input:
+            digitalId = line.split('\t')[0]
+            if digitalId.startswith('Aleph'): #skip header
+                continue
+            url = URL_TEMPLATE % digitalId
+            response = session.get(url)
+            if response.status_code != requests.codes.ok:
+                print('Failed to fetch %d %s' % (response.status_code, url))
+                if response.status_code >= 500:
+                     time.sleep(5)
+                continue
+            doc = ET.HTML(response.content)
+            #print response.content
+            #foo = doc.cssselect('.td1[id^=Related] a')  # '.td1[id^=Related] a' # [@class="td1"]') # contains(@id,"Related"]/a')
+            foo = doc.findall('.//td[@class="td1"]/a[@href]') 
+            href = foo[0].attrib['href']
+            originalId = href.split('=')[-1]
+            print('\t'.join([originalId, digitalId]))
+            
+            
+    pass
+
+if __name__ == '__main__':
+    main()
