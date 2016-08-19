0293f0c06f07520dd106f81b30550389e10fa7ae
Jonathan Reeve <jon.reeve@gmail.com>
2016-08-19 16:08:16 +0900

    more 2->3 changes

--- gitlit/alto.py
+++ gitlit/alto.py
@@ -6,10 +6,6 @@ Created on Jan 20, 2016
 @author: Tom Morris <tfmorris@gmail.com>
 @license: Apache License 2.0
 '''
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
 
 from array import array
 from collections import Counter
