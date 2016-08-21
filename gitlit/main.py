e5f6762a249eec849d0b0b58feeb9a6ae166d91e
Jonathan Reeve <jon.reeve@gmail.com>
2016-08-21 23:46:09 +0900

    fix command hierarchy

--- gitlit/main.py
+++ gitlit/main.py
@@ -10,24 +10,32 @@ import click
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-@click.command()
-def convert(filenames, debug): 
-    """Just converts the books to markdown, without creating a git repository for it."""
-    logging.info('Processing files: %s', filenames) 
-    for filename in filenames: 
-        logging.info('Processing book: %s', filename) 
-        book = BLText(filename)  
-
-
-
-@click.command()
-@click.argument('filenames', nargs=-1) 
+@click.group()
 @click.option('--debug', is_flag=True, help='Turn on debugging mode for more verbose error messages.')
-def cli(filenames, debug): 
+def cli(debug): 
     """Processes books and turns them into GitHub repositories.
     Converts ALTO XML to markdown, adds READMEs, and pushes to GitHub. 
     Only works with British Library compressed ALTO files at the moment. 
     """
+
+@cli.command()
+@click.argument('filenames', nargs=-1) 
+def convert(filenames, debug): 
+    """Just converts the books to markdown, without creating a git repository for it."""
+
+    logging.info('About to convert files: %s', filenames) 
+    for filename in filenames: 
+        logging.info('Converting book: %s', filename) 
+        book = BLText(filename)  
+        with open(book.basename + '.md','w') as f:
+            f.write(book.text + '\n')
+
+@cli.command() 
+@click.argument('filenames', nargs=-1) 
+def process(filenames, debug): 
+    """Processes a book, converting it to markdown and creating a git repository for it,
+    but not pushing it to github."""
+
     logging.info('Processing files: %s', filenames) 
     for filename in filenames: 
         logging.info('Processing book: %s', filename) 
